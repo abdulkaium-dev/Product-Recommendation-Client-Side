77e2d3a089c04e7702d104a8a48faf19b74afa09
Abdulkaium775 <your_email@example.com>
2025-08-31 13:37:47 +0600

    28 commit

--- src/component/DarkModeToggle.jsx
+++ src/component/DarkModeToggle.jsx
@@ -5,7 +5,6 @@ const DarkModeToggle = () => {
     const savedTheme = localStorage.getItem("theme");
     if (savedTheme === "dark") return true;
     if (savedTheme === "light") return false;
-    // Default to light mode if no theme is set
     localStorage.setItem("theme", "light");
     return false;
   });
@@ -14,6 +13,10 @@ const DarkModeToggle = () => {
     const theme = isDark ? "dark" : "light";
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
+
+    // Set page text color
+    document.body.style.color = isDark ? "white" : "black";
+    document.body.style.backgroundColor = isDark ? "#121212" : "#ffffff"; // optional
   }, [isDark]);
 
   const toggleTheme = () => setIsDark(!isDark);
@@ -26,7 +29,8 @@ const DarkModeToggle = () => {
         onChange={toggleTheme}
         className="theme-controller"
       />
-      {/* sun icon - indigo-700 */}
+
+      {/* Sun icon for light mode */}
       <svg
         className="swap-off h-8 w-8 fill-current text-indigo-700"
         xmlns="http://www.w3.org/2000/svg"
@@ -35,9 +39,9 @@ const DarkModeToggle = () => {
         <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
       </svg>
 
-      {/* moon icon - black */}
+      {/* Moon icon for dark mode */}
       <svg
-        className="swap-on h-8 w-8 fill-current text-black"
+        className="swap-on h-8 w-8 fill-current text-white"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
